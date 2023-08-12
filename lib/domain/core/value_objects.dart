2a9b46e174e570c2b1d3e0a5d8122f0fda04e24b
Jafar Pahrudin <jafar.pahrudin@gmail.com>
2023-08-12 13:08:33 +0700

    feat: course[6] - Firebase auth setup & facade

--- lib/domain/core/value_objects.dart
+++ lib/domain/core/value_objects.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:flutter_todo_list/domain/core/errors.dart';
 import 'package:flutter_todo_list/domain/core/failures.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
@@ -7,12 +8,16 @@ abstract class ValueObject<T> {
   const ValueObject();
   Either<ValueFailure<T>, T> get value;
 
+  T getOrCrash() {
+    return value.fold((l) => throw UnexpectedValueError(l), id);
+  }
+
   bool isValid() => value.isRight();
-  
+
   @override
   bool operator ==(Object o) {
     if (identical(this, o)) return true;
-  
+
     return o is ValueObject<T> && o.value == value;
   }
 

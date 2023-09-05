1dba56a4fe989fc095ac3a873b7cddb7266329a2
feliex1992 <jafar.pahrudin@gmail.com>
2023-09-05 18:45:42 +0700

    feat: course [21] Note Cards UI part 2.

--- lib/presentation/notes/notes_overview/widgets/note_card_widget.dart
+++ lib/presentation/notes/notes_overview/widgets/note_card_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_todo_list/application/notes/note_actor/note_actor_bloc.dart';
 import 'package:flutter_todo_list/domain/notes/note.dart';
 import 'package:flutter_todo_list/domain/notes/todo_item.dart';
 import 'package:kt_dart/kt.dart';
@@ -14,30 +16,73 @@ class NoteCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Column(
-        children: [
-          Text(
-            note.body.getOrCrash(),
-            style: const TextStyle(fontSize: 18),
+      color: note.color.getOrCrash(),
+      child: InkWell(
+        onTap: () {
+          // TODO: implement navigation.
+        },
+        onLongPress: () {
+          final noteActorBloc = context.read<NoteActorBloc>();
+          _showDeletionDialog(context, noteActorBloc);
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                note.body.getOrCrash(),
+                style: const TextStyle(fontSize: 18),
+              ),
+              if (note.todos.length > 0) ...[
+                const SizedBox(height: 4),
+                Wrap(
+                  spacing: 8,
+                  children: [
+                    ...note.todos
+                        .getOrCrash()
+                        .map(
+                          (todo) => TodoDisplay(
+                            todo: todo,
+                          ),
+                        )
+                        .iter,
+                  ],
+                )
+              ]
+            ],
           ),
-          if (note.todos.length > 0) ...[
-            const SizedBox(height: 4),
-            Wrap(
-              children: [
-                ...note.todos
-                    .getOrCrash()
-                    .map(
-                      (todo) => TodoDisplay(
-                        todo: todo,
-                      ),
-                    )
-                    .iter,
-              ],
-            )
-          ]
-        ],
+        ),
       ),
     );
+  }
+
+  void _showDeletionDialog(BuildContext context, NoteActorBloc noteActorBloc) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("Selected Note:"),
+            content: Text(
+              note.body.getOrCrash(),
+              maxLines: 3,
+              overflow: TextOverflow.ellipsis,
+            ),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text("CANCEL"),
+              ),
+              TextButton(
+                onPressed: () {
+                  noteActorBloc.add(NoteActorEvent.deleted(note));
+                  Navigator.pop(context);
+                },
+                child: const Text("DELETE"),
+              ),
+            ],
+          );
+        });
   }
 }
 
